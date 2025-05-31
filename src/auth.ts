22c03def065cbcc0c7a1324cf57d4c3e27424a8f
imelkayy <lkkcraft@gmail.com>
2025-05-31 09:14:26 -0700

    Create list of provider names and ids

--- src/auth.ts
+++ src/auth.ts
@@ -5,3 +5,8 @@ import GitHub from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Discord, GitHub],
 })
+
+export const providers = [
+  { name: "Discord", id: "discord" },
+  { name: "GitHub", id: "github" }
+]
