d507d8ed9ed3a190e0ff6aa5917dfa99773f229c
imelkayy <lkkcraft@gmail.com>
2025-05-31 10:11:35 -0700

    Implement authjs prisma adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,13 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/prisma"
+
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
 import GitHub from "next-auth/providers/github"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Discord, GitHub],
+  adapter: PrismaAdapter(prisma)
 })
 
 export const providers = [
