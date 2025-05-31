b3d6652d94b051948778c0243555346558308175
imelkayy <lkkcraft@gmail.com>
2025-05-30 22:43:46 -0700

    Add github authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
+import GitHub from "next-auth/providers/github"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Discord],
+  providers: [Discord, GitHub],
 })
