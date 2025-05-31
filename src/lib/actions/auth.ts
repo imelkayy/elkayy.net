89d580d84f8bbc5e673e3dc11473b42ff91579df
imelkayy <lkkcraft@gmail.com>
2025-05-31 09:16:25 -0700

    Create auth signin action

---
+++ src/lib/actions/auth.ts
@@ -0,0 +1,7 @@
+"use server";
+
+import { signIn } from "@/auth";
+
+export async function dispatchSignin(provider: string, redirect?: string) {
+    return await signIn(provider, { redirect: Boolean(redirect), redirectTo: redirect });
+}
