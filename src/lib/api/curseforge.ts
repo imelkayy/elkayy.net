6b416d893247d830023f6c0437c063e9651944f6
imelkayy <lkkcraft@gmail.com>
2025-05-31 22:00:28 -0700

    Create local FetchedModInfo type

--- src/lib/api/curseforge.ts
+++ src/lib/api/curseforge.ts
@@ -2,6 +2,13 @@ const baseUrl: string = "https://api.curseforge.com";
 const cacheDuration: number = 60 * 60 * 12; // Cache duration of 12 hours
 const cfProvider: string = "curseforge"
 
+type FetchedModInfo = {
+  updatedAt: Date,
+  summary: string,
+  providerUrl: string,
+  description: string
+}
+
 async function fetchCurseforge(route: string) {
   return await fetch(`${baseUrl}/v1${route}`, {
     cache: 'force-cache',
