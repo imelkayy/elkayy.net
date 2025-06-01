81ea5838651ee2af4e4af3748c3e8b041d8dbc28
imelkayy <lkkcraft@gmail.com>
2025-05-31 21:57:48 -0700

    Create curseforge fetch wrapper

---
+++ src/lib/api/curseforge.ts
@@ -0,0 +1,15 @@
+const baseUrl: string = "https://api.curseforge.com";
+const cacheDuration: number = 60 * 60 * 12; // Cache duration of 12 hours
+const cfProvider: string = "curseforge"
+
+async function fetchCurseforge(route: string) {
+  return await fetch(`${baseUrl}/v1${route}`, {
+    cache: 'force-cache',
+    next: {
+      revalidate: cacheDuration
+    },
+    headers: {
+      "x-api-key": process.env.CURSEFORGE_KEY as string
+    }
+  })
+}
