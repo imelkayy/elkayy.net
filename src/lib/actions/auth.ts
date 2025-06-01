55750a1edfe6acfa480eed0f085916fcbe544ce4
imelkayy <lkkcraft@gmail.com>
2025-05-31 19:59:30 -0700

    Update auth action indentation

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -3,5 +3,5 @@
 import { signIn } from "@/auth";
 
 export async function dispatchSignin(provider: string, redirect?: string) {
-    return await signIn(provider, { redirect: Boolean(redirect), redirectTo: redirect });
+  return await signIn(provider, { redirect: Boolean(redirect), redirectTo: redirect });
 }
