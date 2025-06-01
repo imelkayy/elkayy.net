1214c3a191c2b50ad982ff1deb9ef00846f3ecff
imelkayy <lkkcraft@gmail.com>
2025-06-01 12:03:15 -0700

    Enable Auth.js trustHost

--- src/auth.ts
+++ src/auth.ts
@@ -7,7 +7,8 @@ import GitHub from "next-auth/providers/github"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Discord, GitHub],
-  adapter: PrismaAdapter(prisma)
+  adapter: PrismaAdapter(prisma),
+  trustHost: true
 })
 
 export const providers = [
