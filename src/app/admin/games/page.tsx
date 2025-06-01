bcba2c509b4d6e94e1536c78745fc4e014ffda1e
imelkayy <lkkcraft@gmail.com>
2025-06-01 16:59:22 -0700

    Implement /admin/games

--- src/app/admin/games/page.tsx
+++ src/app/admin/games/page.tsx
@@ -1,38 +1,48 @@
-import GamesEditor, { GamesType } from "@/components/admin/gamesEditor";
-import { Typography } from "@mui/material";
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
+import { GamesType } from "@/components/admin/game/gameInput";
+import { prisma } from "@/prisma";
+import GameEditor from "@/components/admin/game/gameEditor";
+import { ScrollItem } from "@/components/admin/scrollMenu";
 
-export default function AdminGamesPage() {
+export default async function AdminGamesPage() {
+  const games = await prisma.game.findMany();
 
-  async function saveGame(value: GamesType) {
+  async function saveGame(id: number | undefined, value: GamesType): Promise<ScrollItem> {
     "use server";
+    const data = {
+      name: value.name,
+      path: value.path
+    };
 
-    console.log(value);
+    if(id)
+      return await prisma.game.upsert({
+        where: { id: id },
+        create: data,
+        update: data
+      })
+    else
+      return await prisma.game.create({
+        data: data
+      })
   }
 
-  return(
-    // TODO: Add selection for games
-    <Box>
-      <Stack
-        spacing={1}
-        maxWidth={"25%"}
-        marginLeft="auto"
-        marginRight="auto"
-      >
-        <Typography
-          variant="h5"
-          textAlign="center"
-        >
-          Edit Game
-        </Typography>
-        
-        <GamesEditor
-          onSave={saveGame}
-        />
-      </Stack>
-      
-    </Box>
-    
+  async function getGame(id: number): Promise<GamesType | null> {
+    "use server";
+    return await prisma.game.findFirst({where: {id: id}});
+  }
+
+  async function removeGame(id: number): Promise<Boolean> {
+    "use server";
+    const deleted = await prisma.game.delete({ where: {id: id} });
+    console.log(deleted);
+    return Boolean(deleted);
+  }
+
+  return (
+    <GameEditor
+      games={games}
+      getGame={getGame}
+      saveGame={saveGame}
+      removeGame={removeGame}
+    />
   )
 }
