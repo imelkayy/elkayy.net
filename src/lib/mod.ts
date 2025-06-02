16ba47f87d9180f3a20e43e8a9bcb955914b2697
imelkayy <lkkcraft@gmail.com>
2025-06-02 15:54:08 -0700

    Modify scrollIdToModId to output a ModKey

--- src/lib/mod.ts
+++ src/lib/mod.ts
@@ -1,10 +1,11 @@
 import { Mod } from "@/generated/prisma";
+import { ModKey } from "./types";
 
 export function getScrollIdForMod(mod: Mod) {
   return `${mod.gameId}.${mod.slug}`;
 }
 
-export function scrollIdToModId(scrollId: string): { slug: string, gameId: number } {
+export function scrollIdToModId(scrollId: string): ModKey {
   const split = scrollId.split(".");
   return {
     slug: split[1],
