dbfa674f731f5ac87b45d689dc24857a4acea250
imelkayy <lkkcraft@gmail.com>
2025-06-02 20:29:14 -0700

    Extract getModInfoFromCF into multiple functions

--- src/lib/api/curseforge.ts
+++ src/lib/api/curseforge.ts
@@ -2,12 +2,13 @@ const baseUrl: string = "https://api.curseforge.com";
 const cacheDuration: number = 60 * 60 * 12; // Cache duration of 12 hours
 const cfProvider: string = "curseforge"
 
-type FetchedModInfo = {
+type ModInfo = {
   updatedAt: Date,
   summary: string,
-  providerUrl: string,
-  description: string
+  providerUrl: string
 }
+
+type ModInfoAndDesc = ModInfo & { description: string }
 
 async function fetchCurseforge(route: string) {
   return await fetch(`${baseUrl}/v1${route}`, {
@@ -21,14 +22,28 @@ async function fetchCurseforge(route: string) {
   })
 }
 
-export async function getModInfoFromCF(modId: number): Promise<FetchedModInfo> {
+export async function getModInfoFromCF(modId: number): Promise<ModInfo> {
   const res = (await (await fetchCurseforge(`/mods/${modId}`)).json()).data;
-  const des = await (await fetchCurseforge(`/mods/${modId}/description`)).json();
 
   return {
     updatedAt: new Date(res.dateModified),
     summary: res.summary,
-    providerUrl: res.links.websiteUrl,
-    description: des.data
+    providerUrl: res.links.websiteUrl
+  }
+}
+
+export async function getDescriptionFromCF(modId: number): Promise<string> {
+  const des = await (await fetchCurseforge(`/mods/${modId}/description`)).json();
+
+  return des.data;
+}
+
+export async function getModInfoAndDescFromCF(modId: number): Promise<ModInfoAndDesc> {
+  const info = await getModInfoFromCF(modId);
+  const des = await getDescriptionFromCF(modId);
+
+  return {
+    ...info,
+    description: des
   };
 }
