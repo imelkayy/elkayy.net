98150c465e7e2631974a476f9f09fb2477d99a0f
imelkayy <lkkcraft@gmail.com>
2025-06-03 08:24:09 -0700

    Render curseforge description on mods page

--- src/app/mods/[game]/[mod]/page.tsx
+++ src/app/mods/[game]/[mod]/page.tsx
@@ -1,6 +1,9 @@
 import { prisma } from "@/prisma";
 import { redirect } from "next/navigation";
 import { GameParam } from "../page";
+import Box from "@mui/material/Box";
+import { Card, Paper, Typography } from "@mui/material";
+import { getDescriptionFromCF } from "@/lib/api/curseforge";
 
 export type GameModParam = GameParam & { mod: string };
 export type ModsPageProps = { params: Promise<GameModParam> };
@@ -27,7 +30,38 @@ export default async function GameModsPage({ params } : ModsPageProps) {
 
   if(!mod) redirect(`/mods/${gamePath}`);
 
+  let description = ""
+
+  if(mod.providerId)
+    description = await getDescriptionFromCF(mod.providerId)
+
   return (
-    <div>{mod.name}</div>
+    <Box>
+      <Typography
+        variant="h3"
+        fontWeight="bold"
+        marginLeft="10px"
+      >
+        {mod.name}
+      </Typography>
+      <Card
+        component={Paper}
+        variant="elevation"
+        sx={{
+          padding: "15px"
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            textTransform: "capitalize"
+          }}
+        >
+          {mod.provider} Description:
+        </Typography>
+
+        <Typography dangerouslySetInnerHTML={{__html: description}} />
+      </Card>
+    </Box>
   )
 }
