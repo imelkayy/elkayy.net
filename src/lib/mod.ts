eabda37c2bd38ef324cfbfc8f1793268b1a03641
imelkayy <lkkcraft@gmail.com>
2025-06-02 20:32:14 -0700

    Create saveAndCacheMod()

--- src/lib/mod.ts
+++ src/lib/mod.ts
@@ -1,5 +1,7 @@
 import { Mod } from "@/generated/prisma";
 import { ModKey } from "./types";
+import { getModInfoFromCF } from "./api/curseforge";
+import { prisma } from "@/prisma";
 
 export function getScrollIdForMod(mod: Mod) {
   return `${mod.gameId}.${mod.slug}`;
@@ -11,4 +13,25 @@ export function scrollIdToModId(scrollId: string): ModKey {
     slug: split[1],
     gameId: Number(split[0])
   };
+}
+
+export async function saveAndCacheMod(key: ModKey | undefined, mod: Mod) {
+  if(mod.providerId) {
+    const modInfo = await getModInfoFromCF(mod.providerId);
+    mod.updatedAt = modInfo.updatedAt;
+    mod.summary = modInfo.summary;
+  }
+  
+  if(key) {
+    await prisma.mod.update({
+      where: {
+        slug_gameId: key
+      },
+      data: mod
+    });
+  } else {
+    await prisma.mod.create({
+      data: mod
+    });
+  }
 }
