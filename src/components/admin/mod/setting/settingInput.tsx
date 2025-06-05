19647b7b3a57669208517c9b5a703c25eeee3ae3
imelkayy <lkkcraft@gmail.com>
2025-06-05 11:36:23 -0700

    Wrap setting inputs in an outlined Paper box

--- src/components/admin/mod/setting/settingInput.tsx
+++ src/components/admin/mod/setting/settingInput.tsx
@@ -2,7 +2,7 @@
 
 import { Setting } from "@/generated/prisma";
 import { SelectOption } from "@/lib/types";
-import { IconButton, TextField, Tooltip } from "@mui/material";
+import { Box, IconButton, Paper, TextField, Tooltip } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import RemoveIcon from '@mui/icons-material/Remove';
 
@@ -26,79 +26,85 @@ export default function SettingInput({
   }
 
   return (
-    <Stack
-      direction="row"
+    <Box
+      component={Paper}
+      padding="10px"
+      variant="outlined"
     >
       <Stack
-        direction="column"
-        spacing={0.75}
+        direction="row"
       >
         <Stack
-          direction="row"
-          spacing={0.5}
+          direction="column"
+          spacing={0.75}
         >
-          <TextField 
-            label="Name"
-            value={setting.name}
-            onChange={e => handleChange({ name: e.target.value })}
-          />
-
-          <TextField 
-            label="Key"
-            value={setting.key}
-            onChange={e => handleChange({ key: e.target.value })}
-          />
-
-          <TextField 
-            label="Default Value"
-            value={setting.default}
-            onChange={e => handleChange({ default: e.target.value })}
-          />
-
-          <TextField 
-            select
-            label="Type"
-            value={setting.type}
-            onChange={e => handleChange({ type: Number(e.target.value) })}
-            error={!typeValid}
-            helperText={typeValid ? "" : "Please select a type."} 
-            slotProps={{
-              select: {
-                native: true
-              }
-            }}
-          > 
-            {types.map((type) => (
-              <option key={type.id} value={type.id}>{type.label}</option>
-            ))}
-          </TextField>
-        </Stack>
-        <TextField 
-          label="Description"
-          value={setting.description}
-          onChange={e => handleChange({ description: e.target.value })}
-        />
-      </Stack>
-
-      {
-        onRemove?
-        <Tooltip
-          title={`Remove ${setting.name}`}
-        >
-          <IconButton
-            sx={{
-              alignSelf: "center"
-            }}
-            onClick={() => onRemove(setting.id)}
-            aria-label={`Remove ${setting.name}`}
+          <Stack
+            direction="row"
+            spacing={0.5}
           >
-            <RemoveIcon />
-          </IconButton>
-        </Tooltip>
-        :
-        <></>
-      }
+            <TextField 
+              label="Name"
+              value={setting.name}
+              onChange={e => handleChange({ name: e.target.value })}
+            />
 
-    </Stack>
+            <TextField 
+              label="Key"
+              value={setting.key}
+              onChange={e => handleChange({ key: e.target.value })}
+            />
+
+            <TextField 
+              label="Default Value"
+              value={setting.default}
+              onChange={e => handleChange({ default: e.target.value })}
+            />
+
+            <TextField 
+              select
+              label="Type"
+              value={setting.type}
+              onChange={e => handleChange({ type: Number(e.target.value) })}
+              error={!typeValid}
+              helperText={typeValid ? "" : "Please select a type."} 
+              slotProps={{
+                select: {
+                  native: true
+                }
+              }}
+            > 
+              {types.map((type) => (
+                <option key={type.id} value={type.id}>{type.label}</option>
+              ))}
+            </TextField>
+          </Stack>
+          <TextField 
+            label="Description"
+            value={setting.description}
+            onChange={e => handleChange({ description: e.target.value })}
+          />
+        </Stack>
+
+        {
+          onRemove?
+          <Tooltip
+            title={`Remove ${setting.name}`}
+          >
+            <IconButton
+              sx={{
+                alignSelf: "center"
+              }}
+              onClick={() => onRemove(setting.id)}
+              aria-label={`Remove ${setting.name}`}
+            >
+              <RemoveIcon />
+            </IconButton>
+          </Tooltip>
+          :
+          <></>
+        }
+
+      </Stack>
+    </Box>
   )
 }
