a0e7bf04ab373ec5eff6da1188c20b5d851a9f50
imelkayy <lkkcraft@gmail.com>
2025-06-04 21:45:19 -0700

    Implement saving of setting changes

--- src/app/admin/mods/page.tsx
+++ src/app/admin/mods/page.tsx
@@ -1,5 +1,5 @@
 import { ModEditor } from "@/components/admin/mod/modEditor";
-import { Mod } from "@/generated/prisma";
+import { Mod, Setting } from "@/generated/prisma";
 import { saveAndCacheMod } from "@/lib/mod";
 import { ModKey } from "@/lib/types";
 import { prisma } from "@/prisma"
@@ -40,12 +40,55 @@ export default async function AdminModsPage() {
     return Boolean(removed)
   }
 
+  async function saveSettings(settings: Setting[], remove: number[], forMod: ModKey) {
+    "use server";
+    console.log(remove);
+
+    if(settings) {
+      settings.forEach(async (setting) => {
+        if(setting.id >= 0) {
+          await prisma.setting.update({
+            where: {
+              id: setting.id
+            },
+            data: {
+              ...setting,
+              modGame: forMod.gameId,
+              modSlug: forMod.slug
+            }
+          });
+        } else {
+          await prisma.setting.create({
+            data: {
+              name: setting.name,
+              default: setting.default,
+              type: setting.type,
+              modGame: forMod.gameId,
+              modSlug: forMod.slug
+            }
+          });
+        }
+      })
+    }
+    // Handle deleted settings
+    if(remove) {
+      remove.forEach(async (settingId) => {
+        await prisma.setting.delete({
+          where: {
+            id: settingId
+          }
+        });
+      })
+    }
+  }
+
   return (
     <ModEditor
       mods={allMods}
       games={games}
       saveMod={saveMod}
       removeMod={removeMod}
+      saveSettings={saveSettings}
     />
   )
 }
