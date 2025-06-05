6b256c3c1a8696a2ca17d4e17ff4e558ce3cfc52
imelkayy <lkkcraft@gmail.com>
2025-06-04 17:30:42 -0700

    Create SettingType_SelectOptions

--- src/lib/enums.ts
+++ src/lib/enums.ts
@@ -1,3 +1,5 @@
+import { SelectOption } from "./types";
+
 export enum UserRoles {
   Default = 0,
   Admin = 1
@@ -12,3 +14,16 @@ export enum SettingType {
   Vector,
   Classes
 }
+
+export const SettingType_SelectOptions: SelectOption[] = Object.entries(SettingType)
+  .filter((t) => {
+    const num = Number(t[0])
+    return !Number.isNaN(num);
+  }).map(
+    t => {
+      return { 
+        id: Number(t[0]), 
+        label: String(t[1]) 
+      }
+  }
+);
