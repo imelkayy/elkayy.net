fdbf01a81c7d7a8a3b475edc28d3b1737cd4383a
imelkayy <lkkcraft@gmail.com>
2025-06-06 13:25:08 -0700

    Create SettingsTable component

---
+++ src/components/mods/settings/settingsTable.tsx
@@ -0,0 +1,40 @@
+import { Setting } from "@/generated/prisma"
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+
+type SettingsTableProps = {
+  settings: Setting[]
+}
+
+export default function SettingsTable({ settings }: SettingsTableProps) {
+
+  return (
+    <TableContainer>
+      <Table
+        size="small"
+      >
+       <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell>Key</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell>Input Type</TableCell>
+          <TableCell>Default Value</TableCell>
+        </TableRow>
+       </TableHead>
+       <TableBody>
+        {
+          settings.map((setting) => (
+            <TableRow key={setting.id}>
+              <TableCell>{setting.name}</TableCell>
+              <TableCell>{setting.key}</TableCell>
+              <TableCell>{setting.description}</TableCell>
+              <TableCell>{setting.type}</TableCell>
+              <TableCell>{setting.default}</TableCell>
+            </TableRow>
+          ))
+        }
+       </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
